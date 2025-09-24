88144c4a7b3befcdfc70c5bae8a5418ab82881a2
mohamed-safouen <mohamedsafouen@gmail.com>
2025-09-24 20:47:16 +0100

    add comment for docker files

--- front/src/App.js
+++ front/src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>all Members </h1>
+        <h1>All Members </h1>
         {blogs && blogs.map(blog => (
           <div key={blog.id}>{blog.title}</div>
         ))}
